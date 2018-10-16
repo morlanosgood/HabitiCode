3e45d6ca7e7d98945aa2a9bc91300decceef7249
morlanosgood <morlano@princeton.edu>
2018-10-16 16:56:09 -0400

    Update leetcode_data.php

--- leetcode_data.php
+++ leetcode_data.php
@@ -1,1 +1,20 @@
+// FITBIT CODE FOR REFERENCE
+<?php
 
+$curl = curl_init();
+$curlArray = array(
+    CURLOPT_RETURNTRANSFER => true, 
+    CURLOPT_HEADER => false,
+    CURLOPT_HTTPHEADER => array(
+        "Content-type: application/json",
+        "Authorization: " . $_GET['token_type']. " " . $_GET['access_token']),
+    CURLOPT_URL => "https://api.fitbit.com/1/user/". $_GET['user_id']."/activities/date/".$_GET['date'].".json");
+
+curl_setopt_array($curl, $curlArray);
+
+$resp = curl_exec($curl);
+
+curl_close($curl);
+echo $resp;
+
+?>
