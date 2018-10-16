949fcab0b195d5a7aed6d6a0432b936d78276ffe
morlanosgood <morlano@princeton.edu>
2018-10-16 17:01:06 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
 <html>
       <head>
-        <title>HabitiCode</title>
+       <title>HabitiCode</title>
+       <script src="js/jquery-2.1.4.min.js"></script>
+       <script src="js/bootstrap.min.js"></script>
+        <script src="js/index.js"></script>
+       <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+       <link rel="stylesheet" href="css/style.css" type="text/css" />
       </head>
       <body>
         HELLOOOOO!!!!
