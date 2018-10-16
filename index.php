ca833f8674b926616a5dfa5f692ce4d0fb0e596d
Morlan Osgood <morlano@princeton.edu>
2018-10-16 17:35:35 -0400

    trying bootstrap

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html5>
+<html lang="en">
       <head>
        <title>HabitiCode</title>
-       <script src="js/jquery-2.1.4.min.js"></script>
-       <script src="js/bootstrap.min.js"></script>
-        <script src="js/index.js"></script>
-       <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
-       <link rel="stylesheet" href="css/style.css" type="text/css" />
+         <script src="js/jquery-2.1.4.min.js"></script>
+         <script src="js/bootstrap.min.js"></script>
+         <script src="js/index.js"></script>
+         <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+         <link rel="stylesheet" href="css/app.css" type="text/css" />
       </head>
       <body>
-        HELLOOOOO!!!!
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              col 1
+            </div>
+            <div class="col">
+              col 2
+            </div>
+            <div class="col">
+              col 3
+            </div>
       </body>
  </html>
