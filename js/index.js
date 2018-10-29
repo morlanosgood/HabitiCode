0d6d18480d56b226b55cdc05dcf587c17b18f686
Morlan Osgood <morlano@princeton.edu>
2018-10-29 18:02:26 -0400

    added comments

--- js/index.js
+++ js/index.js
@@ -24,6 +24,7 @@ $(document).ready(function() {
                          user_stats = false;
                       }else{
                           user_stats = data;
+                          console.log(data);
                       }
                  }
                });
