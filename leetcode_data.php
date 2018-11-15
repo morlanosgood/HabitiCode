a1a3584e5d03f56c97b511981d0a8e478691a103
Morlan Osgood <morlano@princeton.edu>
2018-11-15 15:38:52 -0500

    hard code Cookie

--- leetcode_data.php
+++ leetcode_data.php
@@ -23,11 +23,13 @@ curl_setopt_array($curl, array(
     "x-requested-with: XMLHttpRequest"
   ),
 ));
-$result = curl_exec($curl);
-preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $result, $matches);
+// $result = curl_exec($curl);
+// preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $result, $matches);
 
 //Get Submissions
+
 $curl = curl_init();
+
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://leetcode.com/api/submissions/?offset=0&limit=20&lastkey=",
   CURLOPT_RETURNTRANSFER => true,
@@ -36,23 +38,23 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOP_COOKIEFILE => $cookieJar,
   CURLOPT_HTTPHEADER => array(
-    "Content-type: application/json",
-  //   "accept: */*",
-  //   "accept-language: en-US,en;q=0.5",
-  //   "cache-control: no-cache",
-  //   "connection: keep-alive",
-    "Cookie: __cfduid=d3c20efb0e9e3c294dd9c292f809ad06e1541623644; csrftoken=pawCO11wpFj0pd8LXKGhUx090aJ1RkmqCukTyNZilRaZjpg0JoC3lrZxaNiipkKg; _ga=GA1.2.1371157599.1541623728; _gid=GA1.2.2105353320.1542313597; _gat=1; messages="e1a52cb0d4d1dd2b9afb2061eb89ab322a3620bb$[[\"__json_message\"\0540\05425\054\"Successfully signed in as morlano.\"]]"; LEETCODE_SESSION=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfYXV0aF91c2VyX2lkIjoiMTQzODExOCIsIl9hdXRoX3VzZXJfYmFja2VuZCI6ImRqYW5nby5jb250cmliLmF1dGguYmFja2VuZHMuTW9kZWxCYWNrZW5kIiwiX2F1dGhfdXNlcl9oYXNoIjoiZWZjMTE3NDhhNGU2NWM3Y2I2MzBmMGMyYWRlOWY5MTc1ODRiNjgzZCIsImlkIjoxNDM4MTE4LCJlbWFpbCI6Im1vcmxhbm9AcHJpbmNldG9uLmVkdSIsInVzZXJuYW1lIjoibW9ybGFubyIsInVzZXJfc2x1ZyI6Im1vcmxhbm8iLCJhdmF0YXIiOiJodHRwczovL3d3dy5ncmF2YXRhci5jb20vYXZhdGFyLzk4YTYzOGNmYWI4MWZlMzc4NjlmYzZhNjY4ZWRhYmEwLnBuZz9zPTIwMCIsInRpbWVzdGFtcCI6IjIwMTgtMTEtMTUgMjA6Mjg6MzAuNDI4NDE2KzAwOjAwIiwiUkVNT1RFX0FERFIiOiI2Ni4xODAuMTgyLjEwNyIsIklERU5USVRZIjoiZjRiMzQyNDI3ZGM5M2U2ZTVmZDVjMTRlZjFmYjc0ZWMiLCJfc2Vzc2lvbl9leHBpcnkiOjEyMDk2MDB9.ocNXGfFzhom4zWh6muSWYfMorgnXNSSE9jzzuqMCyOY"
-  //   "host: leetcode.com",
-  //   // "postman-token: 6ef6a0f2-99ac-0fd2-1979-d7c3a74a4ef8",
-  //   "referer: https://leetcode.com/submissions/",
-  //   "user-agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0",
-  // //  "x-newrelic-id: UAQDVFVRGwEAXVlbBAg=",
-  //   "x-requested-with: XMLHttpRequest"
+    "accept: */*",
+    "accept-language: en-US,en;q=0.5",
+    "cache-control: max-age=0",
+    "connection: keep-alive",
+    "cookie: __cfduid=d3c20efb0e9e3c294dd9c292f809ad06e1541623644; csrftoken=pawCO11wpFj0pd8LXKGhUx090aJ1RkmqCukTyNZilRaZjpg0JoC3lrZxaNiipkKg; _ga=GA1.2.1371157599.1541623728; _gid=GA1.2.2105353320.1542313597; _gat=1; messages=\"e1a52cb0d4d1dd2b9afb2061eb89ab322a3620bb$[[\\"__json_message\\"\0540\05425\054\\"Successfully signed in as morlano.\\"]]\"; LEETCODE_SESSION=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfYXV0aF91c2VyX2lkIjoiMTQzODExOCIsIl9hdXRoX3VzZXJfYmFja2VuZCI6ImRqYW5nby5jb250cmliLmF1dGguYmFja2VuZHMuTW9kZWxCYWNrZW5kIiwiX2F1dGhfdXNlcl9oYXNoIjoiZWZjMTE3NDhhNGU2NWM3Y2I2MzBmMGMyYWRlOWY5MTc1ODRiNjgzZCIsImlkIjoxNDM4MTE4LCJlbWFpbCI6Im1vcmxhbm9AcHJpbmNldG9uLmVkdSIsInVzZXJuYW1lIjoibW9ybGFubyIsInVzZXJfc2x1ZyI6Im1vcmxhbm8iLCJhdmF0YXIiOiJodHRwczovL3d3dy5ncmF2YXRhci5jb20vYXZhdGFyLzk4YTYzOGNmYWI4MWZlMzc4NjlmYzZhNjY4ZWRhYmEwLnBuZz9zPTIwMCIsInRpbWVzdGFtcCI6IjIwMTgtMTEtMTUgMjA6Mjg6MzAuNDI4NDE2KzAwOjAwIiwiUkVNT1RFX0FERFIiOiI2Ni4xODAuMTgyLjEwNyIsIklERU5USVRZIjoiZjRiMzQyNDI3ZGM5M2U2ZTVmZDVjMTRlZjFmYjc0ZWMiLCJfc2Vzc2lvbl9leHBpcnkiOjEyMDk2MDB9.ocNXGfFzhom4zWh6muSWYfMorgnXNSSE9jzzuqMCyOY",
+    "host: leetcode.com",
+    "referer: https://leetcode.com/submissions/",
+    "user-agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0",
+    "x-newrelic-id: UAQDVFVRGwEAXVlbBAg=",
+    "x-requested-with: XMLHttpRequest"
   ),
 ));
+
 $response = curl_exec($curl);
+$err = curl_error($curl);
+
 curl_close($curl);
 
 echo $response
