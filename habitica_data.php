f046eade964a2955cf4906a0b5010f00f0e6e0ea
Morlan Osgood <morlano@princeton.edu>
2018-12-03 16:38:33 -0500

    old habit

--- habitica_data.php
+++ habitica_data.php
@@ -8,27 +8,52 @@ $api_tok = $params['api_tok'];
 
 $habit = new Habitica($user_id, $api_tok);
 
-  if ( $action == 'get_stats' )
-  {
-      echo json_encode($habit->userStats());
-      exit;
-  }
+if($action == 'change_habit'){
+    $dir = $params['direction'];
+    $taskName = $params['task_name'];
+    $task_id = $habit->getTaskId($taskName);
 
-  if ( $action == 'check_habit' )
-  {
-    //get taskId
-    $task_name = $params['task_name'];
-    $task_id = $habit->getTaskId($task_name);
-
-    //if task not available, create it
-    if($task_id == 'No task found with that name')
-    {
-      $newTaskParams = array( 'type': 'habit', 'text': $task_name, 'note': 'This task was created with HabitiCode.');
-      $task_id = $habit->newTask($newTaskParams);
+    if(($task_id != 'No task found with that name') && ($dir == 'up' || $dir == 'down')){
+        $params = array('taskId'=>$task_id,'direction'=>$dir);
+        $rval = $habit->taskScoring($params);
+        echo json_encode($rval);
+    }else{
+        echo 'ERROR';
     }
-    echo $task_id;
-    exit;
-  }
+}else if($action == 'get_stats'){
+    echo json_encode($habit->userStats());
+}
+
+// require_once(__DIR__ . '/api/habitica_api.php');
+//
+// $params = $_GET['data_params'];
+// $action = $_GET['action'];
+// $user_id = $params['user_id'];
+// $api_tok = $params['api_tok'];
+//
+// $habit = new Habitica($user_id, $api_tok);
+//
+//   if ( $action == 'get_stats' )
+//   {
+//       echo json_encode($habit->userStats());
+//       exit;
+//   }
+
+  // if ( $action == 'check_habit' )
+  // {
+  //   //get taskId
+  //   $task_name = $params['task_name'];
+  //   $task_id = $habit->getTaskId($task_name);
+  //
+  //   //if task not available, create it
+  //   if($task_id == 'No task found with that name')
+  //   {
+  //     $newTaskParams = array( 'type': 'habit', 'text': $task_name, 'note': 'This task was created with HabitiCode.');
+  //     $task_id = $habit->newTask($newTaskParams);
+  //   }
+  //   echo $task_id;
+  //   exit;
+  // }
   // else if ( $action == 'change_habit' )
   // {
   //   // $dir = $params['direction'];
